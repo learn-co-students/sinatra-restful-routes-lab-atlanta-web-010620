882790c273ed6f55c8f6f32762c84d68039ed683
tylergreason <tyler.greason@protonmail.com>
2020-01-29 08:58:24 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,50 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  # show all 
+  get '/recipes' do 
+    @recipes = Recipe.all 
+    erb :index 
+  end 
+
+  # create new recipe 
+  get '/recipes/new' do 
+    erb :new 
+  end 
+
+  post '/recipes' do 
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
+  end 
+
+  # display by id 
+  get '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show 
+  end 
+
+  # delete recipe 
+  delete '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete 
+    redirect to '/recipes'
+  end 
+
+  # edit recipe 
+  # load edit form 
+  get '/recipes/:id/edit' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit 
+  end 
+
+  # edit the instance 
+  patch '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save 
+    redirect to "/recipes/#{@recipe.id}"
+  end 
 
 end
